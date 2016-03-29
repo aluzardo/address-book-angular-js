70ebae82d480118b4240d2591bf2f5f54b45bb1c
aluzardo <adolfoluzardocabrera@gmail.com>
2016-03-29 17:18:45 +0100

    Using gh-pages

--- app/browserify_modules/country-list.js
+++
@@ -1,7 +0,0 @@
-// This file is only to generate country-list-bundle.js using browserify app/browserify_modules/country-list.js -o app/browserify_modules/country-list-bundle.js
-
-var countries = require('country-list')();
-
-var countriesArray = JSON.stringify(countries.getData());
-
-localStorage.setItem("ls.countriesArray", countriesArray);
