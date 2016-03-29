df192af3ccc18ca1a7f2ded6311db5fef53b69e8
aluzardo <adolfoluzardocabrera@gmail.com>
2016-03-29 16:31:34 +0100

    Add comment to country-list.js

--- app/browserify_modules/country-list.js
+++ app/browserify_modules/country-list.js
@@ -1,3 +1,5 @@
+// This file is only to generate country-list-bundle.js using browserify app/browserify_modules/country-list.js -o app/browserify_modules/country-list-bundle.js
+
 var countries = require('country-list')();
 
 var countriesArray = JSON.stringify(countries.getData());
